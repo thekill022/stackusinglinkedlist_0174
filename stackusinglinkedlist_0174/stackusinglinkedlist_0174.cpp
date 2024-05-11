b8bccf33702d7d8a6e7e2d14a7adb9c99e1d7acd
thekill022 <gibfathoni@gmail.com>
2024-05-11 11:44:33 +0700

    pembuatan method push

--- stackusinglinkedlist_0174/stackusinglinkedlist_0174.cpp
+++ stackusinglinkedlist_0174/stackusinglinkedlist_0174.cpp
@@ -26,6 +26,16 @@ public:
 		//6. inisilisasi nilai awal top sebagai NULL
 		top = NULL;
 	
+	}
+	//7. pembuatan method push
+	int push(int value) {
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "Push Value: " << value << endl;
+		return value;
+	}
 
 };
 
