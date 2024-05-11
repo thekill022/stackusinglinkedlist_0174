55615da94a5140b10f73cbb09cfa79a1cf8664bb
thekill022 <gibfathoni@gmail.com>
2024-05-11 11:41:25 +0700

    inisialisasi variabel data dan node next sebagai member class node

--- stackusinglinkedlist_0174/stackusinglinkedlist_0174.cpp
+++ stackusinglinkedlist_0174/stackusinglinkedlist_0174.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 //1. inisialisasi class node
 class Node {
+public:
+	//2. inisialisasi variabel data dan Node next sebagai member class Node
+	int data;
+	Node* next;
 
 };
 
